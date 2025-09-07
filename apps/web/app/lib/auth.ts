c13ec24a09a2d24c870ac257481ea48085b1618b
Kamal Shrestha <kshrestha1@godaddy.com>
2025-09-07 19:42:33 +0545

    changes

---
+++ apps/web/app/lib/auth.ts
@@ -0,0 +1,20 @@
+
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions = {
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+        }),
+    ],
+    secret: process.env.NEXTAUTH_SECRET,
+    callbacks: {
+        session: ({ session, token, user }: any) => {
+            if (session.user) {
+                session.user.id = token.sub;
+            }
+            return session;
+        },
+    },
+};
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,12 +1,15 @@
-import { Users } from "./components/Users";
-import { ClickButton } from "./components/ClickButton";
 
-export default function Page() {
-  return (
-    <div>
-      <h1 className="text-3xl font-bold bg-red-500">Hello World</h1>
-      <ClickButton />
-      <Users />
-    </div>
-  );
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import { authOptions } from "./lib/auth";
+
+export default async function Page() {
+    const session = await getServerSession(authOptions);
+
+    if (session?.user) {
+        redirect("/dashboard");
+    }
+    else {
+        redirect("/api/auth/signin");
+    }
 }
